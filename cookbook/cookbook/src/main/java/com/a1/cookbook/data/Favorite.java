b1f4900d9e6ef0ba9b9b14c9ae97209208c09205
David Fortich <d4tich@gmail.com>
2022-10-20 16:21:54 -0400

    One more fix to get AppStartup working properly, hadnt implemented getters, setters, and toString on Favorite

--- cookbook/cookbook/src/main/java/com/a1/cookbook/data/Favorite.java
+++ cookbook/cookbook/src/main/java/com/a1/cookbook/data/Favorite.java
@@ -3,11 +3,11 @@ package com.a1.cookbook.data;
 import javax.persistence.*;
 
 @Entity
-@Table(name="CHEF_RECIPE")
+@Table(name="FAVORITE")
 public class Favorite {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="CHEF_RECIPE_ID")
+    @Column(name="FAVORITE_ID")
     private Long id;
 
     @Column(name="RECIPE_ID")
@@ -15,4 +15,37 @@ public class Favorite {
 
     @Column(name="CHEF_ID")
     private Long chefId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(Long recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public Long getChefId() {
+        return chefId;
+    }
+
+    public void setChefId(Long chefId) {
+        this.chefId = chefId;
+    }
+
+    @Override
+    public String toString() {
+        return "Favorite{" +
+                "id=" + id +
+                ", recipeId=" + recipeId +
+                ", chefId=" + chefId +
+                '}';
+    }
 }
