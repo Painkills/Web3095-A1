279fc6139c6a91a1834f51de662a2848f53f1843
David Fortich <d4tich@gmail.com>
2022-10-21 15:26:37 -0400

    Created Favorite List and Favorite List Service, still untested

--- cookbook/cookbook/src/main/java/com/a1/cookbook/data/FavoriteRepo.java
+++ cookbook/cookbook/src/main/java/com/a1/cookbook/data/FavoriteRepo.java
@@ -3,5 +3,5 @@ package com.a1.cookbook.data;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FavoriteRepo extends CrudRepository<Favorite, Long> {
-    Iterable<Recipe> findFavoritesByChefId(Long chefId);
+    Iterable<Favorite> findFavoritesByChefId(Long chefId);
 }
