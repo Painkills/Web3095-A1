39701f8730d3b7c437f093b47ff6668d21c22299
unknown <d4tich@gmail.com>
2022-10-22 12:16:19 -0400

    Tested and made minor fixes to project to ensure both Favorite Recipe and Meal Plan pages work as expected

--- cookbook/cookbook/src/main/java/com/a1/cookbook/web/PlannedMealController.java
+++ cookbook/cookbook/src/main/java/com/a1/cookbook/web/PlannedMealController.java
@@ -2,10 +2,12 @@ package com.a1.cookbook.web;
 
 import com.a1.cookbook.service.PlannedMeal;
 import com.a1.cookbook.service.PlannedMealService;
+import net.bytebuddy.asm.Advice;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,9 +25,10 @@ public class PlannedMealController {
     @RequestMapping(method = RequestMethod.GET)
     public String getPlans(@RequestParam(value = "chef")String chef, @RequestParam(value="date", required = false) String dateString, Model model) {
         Long chefId = Long.parseLong(chef);
-        System.out.println(chefId);
-        System.out.println(dateString);
-        Map<LocalDate, List<PlannedMeal>> mealPlans = this.mealService.getPlannedMealsByIdAndDate(chefId, LocalDate.parse(dateString));
+        LocalDate date;
+        date = (dateString == null)? LocalDate.now() : LocalDate.parse(dateString);
+
+        Map<LocalDate, List<PlannedMeal>> mealPlans = this.mealService.getPlannedMealsByIdAndDate(chefId, date);
         model.addAttribute("mealPlans", mealPlans);
         return "mealPlanner";
     }
